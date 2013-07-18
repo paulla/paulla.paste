b582f331c58dfbae2905c68c3a67f904b35e9490
Cyprien Le Pannérer <cyplp@free.fr>
2013-07-18 21:58:04 +0200

    Adding fields to models

--- paulla/paste/models.py
+++ paulla/paste/models.py
@@ -3,4 +3,6 @@ class Paste(couchdbkit.Document):
     title = couchdbkit.StringProperty()
     content = couchdbkit.StringProperty()
     created = couchdbkit.DateTimeProperty()
+    user = couchdbkit.StringProperty()
+    password = couchdbkit.StringProperty()
     typeContent = couchdbkit.StringProperty()
